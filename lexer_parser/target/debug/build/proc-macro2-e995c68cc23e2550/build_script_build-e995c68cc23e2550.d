fb2d2dee04096844765a7370655551f8d5c1b558
Tobias-vl <144027122+Tobias-vl@users.noreply.github.com>
2025-04-04 10:54:08 +0200

    Delete lexer_parser/target directory
    deleting target folder in lexer_parser, making commiting to github easier

--- lexer_parser/target/debug/build/proc-macro2-e995c68cc23e2550/build_script_build-e995c68cc23e2550.d
+++
@@ -1,5 +0,0 @@
-C:\Users\AskeSW\Desktop\AAU\Semesters\4.semester\project\lexer_parser\lexer_parser\target\debug\build\proc-macro2-e995c68cc23e2550\build_script_build-e995c68cc23e2550.exe: C:\Users\AskeSW\.cargo\registry\src\index.crates.io-6f17d22bba15001f\proc-macro2-1.0.94\build.rs
-
-C:\Users\AskeSW\Desktop\AAU\Semesters\4.semester\project\lexer_parser\lexer_parser\target\debug\build\proc-macro2-e995c68cc23e2550\build_script_build-e995c68cc23e2550.d: C:\Users\AskeSW\.cargo\registry\src\index.crates.io-6f17d22bba15001f\proc-macro2-1.0.94\build.rs
-
-C:\Users\AskeSW\.cargo\registry\src\index.crates.io-6f17d22bba15001f\proc-macro2-1.0.94\build.rs:
