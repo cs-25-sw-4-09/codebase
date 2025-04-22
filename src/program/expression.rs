87b210b187e286d5481b19d3ff8552e3ef125702
Simon Stjernholm <simon@stjernholm.dev>
2025-04-22 14:18:53 +0200

    more

--- src/program/expression.rs
+++ src/program/expression.rs
@@ -2,7 +2,10 @@ use std::{collections::HashMap, error::Error};
 
 use hime_redist::{ast::AstNode, symbols::SemanticElementTrait};
 
-use super::operators::{binaryoperator::BinaryOperator, unaryoperator::UnaryOperator};
+use super::{
+    errors,
+    operators::{binaryoperator::BinaryOperator, unaryoperator::UnaryOperator},
+};
 
 #[derive(Debug)]
 pub enum Expr {
@@ -35,6 +38,11 @@ impl Expr {
         let expr = match expr.get_symbol().name {
             "INTEGER" => Expr::Integer(expr.get_value().unwrap().parse().unwrap()),
             "+" | "-" | "*" | "/" | "%" | "<" | ">" | "<=" | ">=" | "!=" | "==" | "&&" | "||" => {
+                if expr.children_count() != 2 {
+                    return Err(
+                        errors::ASTNodeChildrenCountInvalid(2, expr.children_count()).into(),
+                    );
+                }
                 let lhs = Box::new(Expr::new(expr.child(0))?);
                 let rhs = Box::new(Expr::new(expr.child(1))?);
                 let operator = BinaryOperator::new(expr.get_symbol())?;
@@ -42,6 +50,11 @@ impl Expr {
                 Expr::BinaryOperation { lhs, rhs, operator }
             }
             "!" => {
+                if expr.children_count() != 1 {
+                    return Err(
+                        errors::ASTNodeChildrenCountInvalid(1, expr.children_count()).into(),
+                    );
+                }
                 let uexpr = Box::new(Expr::new(expr.child(0))?);
                 let operator = UnaryOperator::new(expr.get_symbol())?;
 
@@ -53,34 +66,55 @@ impl Expr {
             "IDENTIFIER" => Expr::Variable(expr.get_value().unwrap().into()),
             "BOOLEAN" => Expr::Boolean(expr.get_value().unwrap().parse().unwrap()),
             "FLOAT" => Expr::Float(expr.get_value().unwrap().parse().unwrap()),
-            "color" => Expr::Color(
-                Box::new(Expr::new(expr.child(0))?),
-                Box::new(Expr::new(expr.child(1))?),
-                Box::new(Expr::new(expr.child(2))?),
-                Box::new(Expr::new(expr.child(3))?)
-            ),
-            "FCall" => Expr::FCall {
-                name: expr.child(0).get_value().unwrap().into(),
-                args: expr
-                    .child(1)
-                    .children()
-                    .iter()
-                    .map(|arg| Expr::new(arg))
-                    .collect::<Result<Vec<_>, _>>()?,
-            },
-            "SCall" => Expr::SCall {
-                name: expr.child(0).get_value().unwrap().into(),
-                args: expr
-                    .child(1)
-                    .children()
-                    .iter()
-                    .map(|arg| {
-                        let key: String = arg.child(0).get_value().unwrap().into();
-                        let value = Expr::new(arg.child(1))?;
-                        Ok::<(String, Expr), Box<dyn Error>>((key, value))
-                    })
-                    .collect::<Result<HashMap<_, _>, _>>()?,
-            },
+            "color" => {
+                if expr.children_count() != 4 {
+                    return Err(
+                        errors::ASTNodeChildrenCountInvalid(4, expr.children_count()).into(),
+                    );
+                }
+                Expr::Color(
+                    Box::new(Expr::new(expr.child(0))?),
+                    Box::new(Expr::new(expr.child(1))?),
+                    Box::new(Expr::new(expr.child(2))?),
+                    Box::new(Expr::new(expr.child(3))?),
+                )
+            }
+            "FCall" => {
+                if expr.children_count() != 2 {
+                    return Err(
+                        errors::ASTNodeChildrenCountInvalid(2, expr.children_count()).into(),
+                    );
+                }
+                Expr::FCall {
+                    name: expr.child(0).get_value().unwrap().into(),
+                    args: expr
+                        .child(1)
+                        .children()
+                        .iter()
+                        .map(|arg| Expr::new(arg))
+                        .collect::<Result<Vec<_>, _>>()?,
+                }
+            }
+            "SCall" => {
+                if expr.children_count() != 2 {
+                    return Err(
+                        errors::ASTNodeChildrenCountInvalid(2, expr.children_count()).into(),
+                    );
+                }
+                Expr::SCall {
+                    name: expr.child(0).get_value().unwrap().into(),
+                    args: expr
+                        .child(1)
+                        .children()
+                        .iter()
+                        .map(|arg| {
+                            let key: String = arg.child(0).get_value().unwrap().into();
+                            let value = Expr::new(arg.child(1))?;
+                            Ok::<(String, Expr), Box<dyn Error>>((key, value))
+                        })
+                        .collect::<Result<HashMap<_, _>, _>>()?,
+                }
+            }
             _ => panic!("Expression type not found: {}", expr.get_symbol().name),
         };
 
