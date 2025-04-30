4b4ff578df71dfeef11e935e35405523f0433209
Christian Steen Nielsen <cniel23@student.aau.dk>
2025-04-30 14:48:21 +0200

    Added genereal test for typechecker

---
+++ src/typechecker/tests/whole_program_test.rs
@@ -0,0 +1,18 @@
+use crate::{program::program::Program, typechecker::TypeCheckP};
+
+use super::valid_programs::get_programs;
+
+/* Example Program parsing */
+#[test]
+fn example_program_parsing() {
+    //A program is succesfully parsed if get_root() does not panic.
+    get_programs()
+    .into_iter()
+    .for_each(|el| {
+        let string = el.to_string();
+        let mut program = Program::new(&string).unwrap();
+        //println!("{:?}",program.type_check().err());
+        assert!(program.type_check().is_ok())
+        
+    });
+}
