eeea4120b9fae927c8995ee3152be5a8ed4e3c71
Simon Stjernholm <simon@stjernholm.dev>
2025-05-09 09:54:45 +0200

    works

--- src/interpreter/utils/manipulation.rs
+++ src/interpreter/utils/manipulation.rs
@@ -1,24 +1,28 @@
-use std::error::Error;
+use crate::interpreter::value::Value;
 use crate::{
     interpreter::{
-        data_types::{figure::Figure, figurearray::FigureArray }, errors::{MaxCanNotBeFound, MinCanNotBeFound}
-    }, 
-    program::{expression::Expr, operators::pathoperator::PathOperator}};
-use crate::interpreter::value::Value;
+        data_types::{figure::Figure, figurearray::FigureArray},
+        errors,
+    },
+    program::{expression::Expr, operators::pathoperator::PathOperator},
+};
+use std::error::Error;
 
+pub fn scale(mut shape: FigureArray, factor: Value) -> Result<(), Box<dyn Error>> {
+    let origin_x = shape
+        .get_mut_lines()
+        .iter()
+        .map(|l| l.get_max_x())
+        .max_by(|a, b| a.partial_cmp(b).unwrap())
+        .ok_or_else(|| Box::new(errors::MinCanNotBeFound))?;
+    let origin_y = shape
+        .get_mut_lines()
+        .iter()
+        .map(|l| l.get_min_y())
+        .min_by(|a, b| a.partial_cmp(b).unwrap())
+        .ok_or_else(|| Box::new(errors::MaxCanNotBeFound))?;
 
-pub fn scale(mut shape: FigureArray, factor: Value) -> Result<(),Box<dyn Error>> { 
-    let origin_x = shape.get_mut_lines().iter().map(|l| l.get_max_x()).max_by(|a,b| a.partial_cmp(b).unwrap_or(std::cmp::Ordering::Equal)).ok_or_else(||Err(MinCanNotBeFound))?;
-    let origin_y = shape.get_mut_lines().iter().map(|l| l.get_min_y()).min_by(|a,b| a.partial_cmp(b).unwrap_or(std::cmp::Ordering::Equal)).ok_or_else(||Err(MaxCanNotBeFound))?;
-
-    for line in shape.get_mut_lines() { 
-
-    }
+    for line in shape.get_mut_lines() {}
 
     Ok(())
-
-
-    }
-  
-
-
+}
