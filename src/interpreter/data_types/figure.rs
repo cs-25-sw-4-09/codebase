82bbe344e49efd925aee563dc24d6c9723cdb3f9
LasseOergaardAAU <lorgaa23@student.aau.dk>
2025-05-09 14:30:24 +0200

    Init stucture, lines done but att WIP

--- src/interpreter/data_types/figure.rs
+++ src/interpreter/data_types/figure.rs
@@ -1,12 +1,12 @@
-use crate::{interpreter::errors, program::expression::Expr};
-use std::{collections::HashMap, error::Error};
 use super::point::Point;
 use crate::interpreter::value::Value;
+use crate::{interpreter::errors, program::expression::Expr};
+use std::{collections::HashMap, error::Error};
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct Figure {
     lines: Vec<Line>,
-    attributes: HashMap<String, Value> 
+    attributes: HashMap<String, Value>,
 }
 
 impl From<Vec<Line>> for Figure {
@@ -21,12 +21,16 @@ impl Figure {
     pub fn new() -> Self {
         Self {
             lines: Vec::new(),
-            attributes: HashMap::new()
+            attributes: HashMap::new(),
         }
     }
 
     pub fn set_attribute(&mut self, attribute: (String, Value)) {
         self.attributes.insert(attribute.0, attribute.1);
+    }
+
+    pub fn get_attributes(&mut self) -> &HashMap<String, Value> {
+        &self.attributes
     }
 
     pub fn push_points(&mut self, ps: Vec<Point>) {
@@ -53,114 +57,149 @@ impl Figure {
     }
 
     pub fn get_max_x(&self) -> i64 {
-        let mut max_x = self.lines.iter()
-       .flat_map(|line| line.get_points())
-       .filter_map(|point| match point.x(){
-        Value::Integer(i) => Some(*i),
-        _ => None,
-       }).max().unwrap_or(0);
-       max_x
+        let mut max_x = self
+            .lines
+            .iter()
+            .flat_map(|line| line.get_points())
+            .filter_map(|point| match point.x() {
+                Value::Integer(i) => Some(*i),
+                _ => None,
+            })
+            .max()
+            .unwrap_or(0);
+        max_x
     }
 
-    pub fn get_min_x(&self) -> i64 { 
-        let mut min_x = self.lines.iter()
-       .flat_map(|line| line.get_points())
-       .filter_map(|point| match point.x(){
-        Value::Integer(i) => Some(*i),
-        _ => None,
-       }).min().unwrap_or(0);
-       min_x
+    pub fn get_min_x(&self) -> i64 {
+        let mut min_x = self
+            .lines
+            .iter()
+            .flat_map(|line| line.get_points())
+            .filter_map(|point| match point.x() {
+                Value::Integer(i) => Some(*i),
+                _ => None,
+            })
+            .min()
+            .unwrap_or(0);
+        min_x
     }
 
     pub fn get_height(&self) -> i64 {
-       self.get_max_x() - self.get_min_x()
+        self.get_max_x() - self.get_min_x()
     }
 
     pub fn get_max_y(&self) -> i64 {
-        let mut max_y = self.lines.iter()
-        .flat_map(|line| line.get_points())
-        .filter_map(|point| match point.y(){
-         Value::Integer(i) => Some(*i),
-         _ => None,
-        }).max().unwrap_or(0);
+        let mut max_y = self
+            .lines
+            .iter()
+            .flat_map(|line| line.get_points())
+            .filter_map(|point| match point.y() {
+                Value::Integer(i) => Some(*i),
+                _ => None,
+            })
+            .max()
+            .unwrap_or(0);
         max_y
     }
 
     pub fn get_min_y(&self) -> i64 {
-        let mut min_y = self.lines.iter()
-        .flat_map(|line| line.get_points())
-        .filter_map(|point| match point.y(){
-         Value::Integer(i) => Some(*i),
-         _ => None,
-        }).min().unwrap_or(0);
+        let mut min_y = self
+            .lines
+            .iter()
+            .flat_map(|line| line.get_points())
+            .filter_map(|point| match point.y() {
+                Value::Integer(i) => Some(*i),
+                _ => None,
+            })
+            .min()
+            .unwrap_or(0);
 
         min_y
     }
 
     pub fn get_width(&self) -> i64 {
         self.get_max_y() - self.get_min_y()
-     }
+    }
 
-     pub fn get_last_line(&mut self) -> Result<&mut Line, Box<dyn Error>> {
-         self.lines.last_mut().ok_or_else(|| errors::NoLinesInFigure.into())
-     }
-     pub fn get_first_line(&mut self) -> Result<&mut Line, Box<dyn Error>> {
-         self.lines.first_mut().ok_or_else(|| errors::NoLinesInFigure.into())
-     }
- 
-     pub fn pop_first_line(&mut self) -> Result<Line, Box<dyn Error>>{
-         if self.lines.is_empty() {
-             Err(errors::NoLinesInFigure.into())
-         } else {
-             Ok(self.lines.remove(0))
-         }
-     }
-     pub fn pop_last_line(&mut self) -> Result<Line, Box<dyn Error>>  {
-         self.lines.pop().ok_or_else(|| errors::NoLinesInFigure.into())
-     }
+    pub fn get_last_line(&mut self) -> Result<&mut Line, Box<dyn Error>> {
+        self.lines
+            .last_mut()
+            .ok_or_else(|| errors::NoLinesInFigure.into())
+    }
+    pub fn get_first_line(&mut self) -> Result<&mut Line, Box<dyn Error>> {
+        self.lines
+            .first_mut()
+            .ok_or_else(|| errors::NoLinesInFigure.into())
+    }
 
+    pub fn pop_first_line(&mut self) -> Result<Line, Box<dyn Error>> {
+        if self.lines.is_empty() {
+            Err(errors::NoLinesInFigure.into())
+        } else {
+            Ok(self.lines.remove(0))
+        }
+    }
+    pub fn pop_last_line(&mut self) -> Result<Line, Box<dyn Error>> {
+        self.lines
+            .pop()
+            .ok_or_else(|| errors::NoLinesInFigure.into())
+    }
 
+    pub fn is_closed(mut self) -> Result<bool, Box<dyn Error>> {
+        let first_point = {
+            let line_ref = self.get_first_line()?;
+            let point_ref = line_ref.get_first_point()?;
+            point_ref.clone()
+        };
+
+        let last_point = {
+            let line_ref = self.get_last_line()?;
+            let point_ref = line_ref.get_last_point()?;
+            point_ref.clone()
+        };
+
+        Ok(first_point == last_point)
+    }
 }
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum Line {
     Straight(Vec<Point>),
-    Curved(Vec<Point>)
+    Curved(Vec<Point>),
 }
 impl Line {
-    pub fn get_points(&self) -> &Vec<Point> { match self {
-        Line::Straight(points) | 
-        Line::Curved(points) => &points,
-    }  }
-    pub fn push_point(&mut self, val: Point) { 
+    pub fn get_points(&self) -> &Vec<Point> {
         match self {
-            Line::Straight(points) | 
-            Line::Curved(points) => points.push(val),
-        } 
+            Line::Straight(points) | Line::Curved(points) => &points,
+        }
+    }
+    pub fn push_point(&mut self, val: Point) {
+        match self {
+            Line::Straight(points) | Line::Curved(points) => points.push(val),
+        }
     }
     pub fn get_last_point(&self) -> Result<&Point, Box<dyn Error>> {
         match self {
-            Line::Straight(points) | 
-            Line::Curved(points) => points.last().ok_or_else(|| errors::NoLinesInFigure.into())
+            Line::Straight(points) | Line::Curved(points) => {
+                points.last().ok_or_else(|| errors::NoLinesInFigure.into())
+            }
         }
     }
     pub fn get_first_point(&self) -> Result<&Point, Box<dyn Error>> {
         match self {
-            Line::Straight(points) | 
-            Line::Curved(points) => points.first().ok_or_else(|| errors::NoLinesInFigure.into())
+            Line::Straight(points) | Line::Curved(points) => {
+                points.first().ok_or_else(|| errors::NoLinesInFigure.into())
+            }
         }
     }
     pub fn insert_point_first(&mut self, p: Point) {
         match self {
-            Line::Straight(points) | 
-            Line::Curved(points) => points.insert(0, p),
+            Line::Straight(points) | Line::Curved(points) => points.insert(0, p),
         }
     }
     pub fn insert_point_last(&mut self, p: Point) {
         match self {
-            Line::Straight(points) | 
-            Line::Curved(points) => points.push(p)
+            Line::Straight(points) | Line::Curved(points) => points.push(p),
         }
     }
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,49 @@
-use std::{error::Error, path::Path};
+use std::{env, error::Error, path::Path};
 
 use codebase::{
-    interpreter::InterpretP, lexer_parser::{
+    generators::generator::{self, get_generator},
+    interpreter::InterpretP,
+    lexer_parser::{
         grammar::cfg,
         //valid_programs::get_programs
         utils::tree_converter::stringify_tree,
-    }, program::program::Program, typechecker::TypeCheckP
+    },
+    program::program::Program,
+    typechecker::TypeCheckP,
 };
 
 fn main() -> Result<(), Box<dyn Error>> {
+    //Lexer/Parser
     let mut program = Program::from_file(Path::new("main.extension"))?;
+
+    //Typecheck
     if let Err(err) = program.type_check() {
         println!("[Typechecker] error: {}", err);
-        return Err(err)
+        return Err(err);
     }
     program.tenvironment.clear();
     println!("[Typechecker] OK");
 
+    //Interpret
     match program.interpret() {
         Ok(_) => println!("[Interpreter] OK"),
         Err(err) => println!("[Interpreter] error: {}", err),
     }
 
-    println!("{:#?}", program.ienvironment);
+    //Generate Files from draw array
+
+    let format = "svg";
+
+    if let Some(generator) = get_generator(format) {
+        if let Err(err) = generator.generate(program.ienvironment.darray_get()) {
+            println!("Failed to generate format: {}, err: {}", format, err);
+        }
+    } else {
+        println!("Unsupported format: {}", format);
+    }
+
+
+    //println!("{:#?}", program.ienvironment);
+
     Ok(())
 }
