6b3ec674a63c63683eeb01cd12c9edb68263f8fd
aske-design1 <hm64zk@student.aau.dk>
2025-05-10 12:52:18 +0200

    finished line

--- src/interpreter/tests/data_types.rs
+++ src/interpreter/tests/data_types.rs
@@ -1,5 +1,5 @@
-use crate::interpreter::data_types::point::Point;
-
+use crate::interpreter::data_types::{line::Line, point::Point, figure::Figure};
+use super::*;
 
 
 /***** Point ******/
@@ -17,19 +17,36 @@ fn point_subtraction() {
 }
 
 
-/***** Line ******/ //todo
+/***** Line ******/
+#[test]
+fn get_first_and_last_point() {
+    let i1 = Line::Curved(vec![(0,0).into(), (5,5).into(), (10,10).into()]);
+    assert_eq!(i1.get_first_point().unwrap().clone(), (0,0).into());
+    assert_eq!(i1.get_last_point().unwrap().clone(), (10,10).into());
+}
 
-
-
+#[test]
+fn insert_first_and_last_point() {
+    let mut i1 = Line::Curved(vec![(0,0).into(), (5,5).into(), (10,10).into()]);
+    i1.insert_point_first((20,20).into());
+    assert_eq!(i1.get_first_point().unwrap().clone(), (20,20).into());
+    i1.insert_point_last((50,50).into());
+    assert_eq!(i1.get_last_point().unwrap().clone(), (50,50).into());
+}
 
  
 /***** Figure ******/ //todo
+#[test]
+fn test_height_width() {}
 
+#[test]
+fn top_left() {}
 
+#[test]
+fn max_min() {}
 
-
-
-
+#[test] 
+fn get_pop_last_first_line() {}
 
 
 /***** Figure Array ******/ //todo
