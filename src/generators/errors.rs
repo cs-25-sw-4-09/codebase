a59cc0ab1f6cb9f169a6e05ad2c82f71a972b428
LasseOergaard <lasse301003@gmail.com>
2025-05-16 13:26:36 +0200

    fix todo in gen

--- src/generators/errors.rs
+++ src/generators/errors.rs
@@ -9,3 +9,12 @@ impl fmt::Display for AttributeNotValid {
         write!(f, "Attribute {} not valid", self.0)
     }
 }
+
+#[derive(Debug, Clone)]
+pub struct TooManyPoints(pub String);
+impl Error for TooManyPoints {}
+impl fmt::Display for TooManyPoints {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "SVG only supports bezier curves with a maximum of 4 points. Amount used: {}", self.0)
+    }
+}
